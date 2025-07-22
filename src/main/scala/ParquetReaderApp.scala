019eb9a65ed918e3e0480238fb9f37c2274542f2
gezever <gezever@skynet.be>
2025-07-22 17:16:20 +0200

    ParquetReaderApp.scala

--- src/main/scala/ParquetReaderApp.scala
+++ src/main/scala/ParquetReaderApp.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
-
+import org.apache.spark.sql.types._
 import scala.io.Source
 
 object ParquetReaderApp {
@@ -11,8 +11,23 @@ object ParquetReaderApp {
       .getOrCreate()
 
     // Read JSON file
-    val jsonFilePath = "src/main/resources/all.json"
+    val jsonFilePath = "src/main/resources/input.json"
+    val schema = StructType(Seq(
+      StructField("_id", StringType, nullable = false),
+      StructField("_type", StringType, nullable = false),
+      StructField("hasFeatureOfInterest", StringType, nullable = false),
+      StructField("observedProperty", StringType, nullable = false),
+      StructField("result", StructType(Seq(
+        StructField("_type", StringType, nullable = false),
+        StructField("eenheid", StringType, nullable = true),
+        StructField("kwalitatieve_waarde", StringType, nullable = true),
+        StructField("numerieke_waarde", DoubleType, nullable = true)
+      ))),
+      StructField("resultTime", TimestampType, nullable = false)
+    ))
+
     val jsonDF: DataFrame = spark.read
+      .schema(schema)
       .option("multiline", "true") // use this if your JSON file spans multiple lines
       .json(jsonFilePath)
 
@@ -32,6 +47,13 @@ object ParquetReaderApp {
       .parquet(parquetOutputPath)
 
     println(s"Data written to Parquet at: $parquetOutputPath")
+
+   val df: DataFrame = spark.read.parquet("src/main/resources/all_parquet")
+
+    println("== Parquet Schema ==")
+   df.printSchema()
+    println("== Parquet Data Preview ==")
+  df.show()
 
     spark.stop()
   }
