89b574d8d6309d6ff7d8623f80c7376f1a257f6e
gezever <gezever@skynet.be>
2025-07-28 13:25:20 +0200

    Alarm validatie

--- src/main/scala/AlarmValidator.scala
+++ src/main/scala/AlarmValidator.scala
@@ -9,105 +9,54 @@ import org.apache.jena.shacl.{ShaclValidator, Shapes}
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-import java.io.File
 
 
 import java.io.{ByteArrayOutputStream, FileOutputStream, FileWriter}
-import scala.collection.convert.ImplicitConversions.`map AsJavaMap`
-//import org.topbraid.shacl.validation.ValidationReport
 import scala.collection.JavaConverters._
 
 import java.io.FileInputStream
 
 object AlarmValidator {
   def main(args: Array[String]): Unit = {
+    // 1. Bestanden
+    val SHAPES = "src/main/resources/be/vlaanderen/omgeving/lzs/alarmValidator/shacl.ttl"
+    val DATA = "src/main/resources/be/vlaanderen/omgeving/lzs/alarmValidator/input.jsonld"
 
-    val SHAPES = "src/main/resources/shacl.ttl"
-    val DATA = "src/main/resources/input.jsonld"
-    val ALARM = "src/main/resources/alarm_output.jsonld"
-    val ALARM_TTL = "src/main/resources/alarm_output.ttl"
-    //val context = new ObjectMapper().readTree(new FileInputStream("src/main/resources/be/vlaanderen/omgeving/lzs/context.json"))
-    val frameFile = "src/main/resources/input.json"
-    val frameString = scala.io.Source.fromFile("src/main/resources/be/vlaanderen/omgeving/lzs/frame.json", "utf-8").getLines.mkString
+    val frameString = scala.io.Source.fromFile("src/main/resources/be/vlaanderen/omgeving/lzs/alarmValidator/frame.json", "utf-8").getLines.mkString
+    val constructAlarmFromValidationResult = scala.io.Source.fromFile("src/main/resources/be/vlaanderen/omgeving/lzs/alarmValidator/constructAlarmFromValidationResult.rq", "utf-8").getLines.mkString
 
+    val ALARM_JSONLD = "output/alarmValidator/alarm_output.jsonld"
+    val ALARM_TTL = "output/alarmValidator/alarm_output.ttl"
 
-
+    // 2. Validatie jsonld tov. shacl => resultaat = report
     val shapesGraph = RDFDataMgr.loadGraph(SHAPES)
     val dataGraph = RDFDataMgr.loadGraph(DATA)
-
     val shapes = Shapes.parse(shapesGraph)
-
     val report = ShaclValidator.get.validate(shapes, dataGraph)
-    //ShLib.printReport(report)
-    System.out.println()
-    //RDFDataMgr.write(System.out, report.getModel, Lang.TTL)
+    //System.out.println()
 
+    // 3. Construct alarm observatie
+    // 3.1. Combineer Data en report in 1 Model
     val observatieModel = ModelFactory.createDefaultModel()
-
-
     RDFParser.create()
       .source(new FileInputStream(DATA))
       .lang(Lang.JSONLD)
       .parse(observatieModel)
-
-    //val combinedModel: Model = observatieModel.union(report.getModel)
     val combinedModel = ModelFactory.createUnion(observatieModel, report.getModel)
-    combinedModel.write(System.out, "TURTLE")
+    //combinedModel.write(System.out, "TURTLE")
 
-    val constructQueryStr =
-      """
-        PREFIX sh: <http://www.w3.org/ns/shacl#>
-        PREFIX sosa: <http://www.w3.org/ns/sosa/>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX observatie: <https://data.lzs.omgeving.vlaanderen.be/id/observation/>
-        PREFIX prov: <http://www.w3.org/ns/prov#>
-        CONSTRUCT {
-          ?newObs a sosa:Observation, ?resultSeverity;
-          prov:wasInformedBy  ?focusNode ;
-          sosa:hasFeatureOfInterest ?hasFeatureOfInterest;
-          sosa:observedProperty      ?observedProperty ;
-          sosa:resultTime ?now ;
-          sosa:phenomenonTime ?tijd;
-          sosa:hasResult   [ rdf:type   sosa:Result;
-                             rdf:value  ?message ];
-           .
-        }
-        WHERE {
-          ?v a sh:ValidationResult ;
-             sh:focusNode ?focusNode ;
-             sh:resultSeverity ?resultSeverity;
-             sh:resultMessage ?message .
-          ?focusNode sosa:hasFeatureOfInterest ?hasFeatureOfInterest;
-              sosa:resultTime ?tijd ;
-              sosa:observedProperty      ?observedProperty.
-
-        BIND(STR(?focusNode) AS ?valStr)
-        BIND(STR(?tijd) AS ?tijdStr)
-        BIND(CONCAT(?valStr, "-", ?tijdStr) AS ?rawStr)
-        BIND(MD5(?rawStr) AS ?hash)
-        BIND(IRI(CONCAT("https://data.lzs.omgeving.vlaanderen.be/id/observation/", ?hash)) AS ?newObs)
-        BIND(NOW() AS ?now)
-               }
-        """.stripMargin
-
-
-
-    // Compileer de query
-    val query: Query = QueryFactory.create(constructQueryStr)
-
-    // Voer de CONSTRUCT uit op het rapportmodel
+    // 3.2. Compileer de query
+    val query: Query = QueryFactory.create(constructAlarmFromValidationResult)
+    // 3.3. Voer de CONSTRUCT uit op het rapportmodel
     val qexec: QueryExecution = QueryExecutionFactory.create(query, combinedModel)
-
     val resultModel: Model = try {
       qexec.execConstruct()
     } finally {
       qexec.close()
     }
 
-    // Schrijf het resultaat naar stdout of sla het op
+    // 3.4. Serialiseer het query-resultaat naar een output TURTLE bestand
     resultModel.write(System.out, "TURTLE")
-
     val output = new FileOutputStream(ALARM_TTL)
     try {
       resultModel.write(output, "TURTLE") // Other formats: "RDF/XML", "N-TRIPLES", "TURTLE"
@@ -115,6 +64,7 @@ object AlarmValidator {
       output.close()
     }
 
+    // 3.5. Serialiseer het query-resultaat naar JSONLD en FRAME dit
     val out = new ByteArrayOutputStream()
     resultModel.write(out, "JSON-LD")
     val jsonldString = out.toString("UTF-8")
@@ -123,40 +73,43 @@ object AlarmValidator {
     val options: JsonLdOptions = new JsonLdOptions
     val framed: java.util.Map[String, Object] = JsonLdProcessor.frame(jsonObject, frame, options)
 
-    val outputFile = new FileWriter(ALARM)
+    // 3.6 Schijf geframed jsonld naar een bestand
+    val outputFile = new FileWriter(ALARM_JSONLD)
     try {
       JsonUtils.writePrettyPrint(outputFile, framed) // or JsonUtils.write(outputFile, framed)
     } finally {
       outputFile.close()
     }
 
+    // 4. Parse @graph value uit jsonld, lees dit in een spark dataframe en schrijf dit weg naar parquet
     val spark = SparkSession.builder()
       .appName("JsonLDToParquet")
       .master("local[*]")
       .getOrCreate()
-
+    // 4.1. parse @graph
     val mapper = new ObjectMapper()
     mapper.registerModule(DefaultScalaModule)
     val rootNode: JsonNode = mapper.readTree(JsonUtils.toPrettyString(framed))
     val graphArray: JsonNode = rootNode.get("@graph")
 
-    // 4. Converteer @graph-array naar een Seq[Map[String, Any]] voor Spark
+    // 4.2. Converteer @graph-array naar een scala Map voor Spark
     val records = graphArray.elements().asScala.map { node =>
       mapper.convertValue[Map[String, Any]](node, classOf[Map[String, Any]])
     }.toSeq
 
-    // 5. Lees in Spark DataFrame
+    // 4.3. Lees in Spark DataFrame
     import spark.implicits._
-    val df: DataFrame = spark.read.json(graphArray.toString)
+    val df: DataFrame = spark.read.json(spark.createDataset(records.map(mapper.writeValueAsString)))
 
-    //val df: DataFrame = spark.read.json(spark.createDataset(records.map(mapper.writeValueAsString)))
-
-    // 6. Toon (optioneel)
+    // 4.4. Toon (optioneel)
     df.printSchema()
     df.show(truncate = false)
 
-    // 7. Schrijf als Parquet
-    df.write.mode("overwrite").parquet("output/graph-data.parquet")
+    // 4.5. Schrijf als Parquet
+    df.coalesce(1).write.mode("overwrite").parquet("output/alarmValidator/alarmValidator_parquet")
+    //Spark schrijft de Parquet-bestanden per partitie ; onderstaande lijn maakt 2 parquetbestanden
+    //df.write.mode("overwrite").parquet("output/graph-data.parquet")
+    //
     //JsonUtils.toPrettyString(framed)
 
   }
